bd5b4c8f0be087294741d50fc2c776992372cf7c
Ahad Memon <ahadmemon2423@gmail.com>
2026-01-13 01:33:26 +0530

    Connect MongoDb Database and install necessary dependecnies

--- server/ehrServer/src/app.js
+++ server/ehrServer/src/app.js
@@ -1,9 +1,22 @@
-import express from "express"
-const app = express()
-const port = process.env.PORT || 4000
+// require("dotenv").config({ path: "./env" });
+import express from "express";
+// import dotenv from "dotenv";
+import DBConnect from "./db/index.js";
+import cors from "cors";
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(cors());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded());
+// app.use(express.static());
+// app.use(cookieParser());
+// dotenv.config({ path: "./env" });
+DBConnect();
 app.get("/user", (req, res) => {
     res.send("Hello this is from user");
-
-})
-app.listen(port, () => console.log("Server started at port ", port)
-)
+});
+app.on("error", (err) => {
+    console.log(err);
+    throw err;
+});
+app.listen(port, () => console.log("Server started at port ", port));
---
+++ server/ehrServer/src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const DBConnect = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URL}`
+        );
+        // if possible givemongoose.connect(
+        //     `${process.env.MONGODB_URL}/${DB_NAME}`
+        console.log(`Connection details ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED: ", error);
+    }
+};
+
+export default DBConnect;
