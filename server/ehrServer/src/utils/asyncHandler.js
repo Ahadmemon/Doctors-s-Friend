b5714bfdac4c80aadd6e8ecc95c5ee5be6ba9fa9
Ahad Memon <ahadmemon2423@gmail.com>
2026-01-22 01:38:10 +0530

    Create Api error and response format

---
+++ server/ehrServer/src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) => (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+};
+export { asyncHandler };
+// const asyncHandler =()=>{}
+// const asyncHandler =(func) => () => {}
+// const asyncHandler =(func) => async () => {}
+// above 3 lines explains what below 3 lines does
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message,
+//         });
+//     }
+// });
